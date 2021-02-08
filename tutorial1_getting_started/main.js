7edf70f10bd80393232c951fd51385b8d27ad837
YuyingRen <renyuying4@gmail.com>
2021-02-07 21:09:48 -0500

    added some javasript

---
+++ tutorial1_getting_started/main.js
@@ -0,0 +1,24 @@
+alert("Welcome to Interactive-Data-Vis-Sp21");
+function addition(a, b) {
+    return a + b;
+  }
+const arrowAddition = (a, b) => a + b;
+console.log('hello world!');
+console.log('-----');
+console.log('addition function:');
+console.log(addition);
+console.log('-----');
+console.log('addition arrow function:')
+console.log(arrowAddition)
+console.log('-----')
+console.log('4 + 5:')
+console.log(addition(4, 5))
+console.log('-----')
+console.log('4 + 5:')
+console.log(4 + 5)
+console.log('-----')
+const appendWahoo = (string) => string + ' wahoo!!!'
+
+console.log(appendWahoo('I love this class'))
+console.log(appendWahoo('javascript is so fun'))
+document.write("This class is fun!");
