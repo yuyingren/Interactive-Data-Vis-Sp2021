4688d226ae604a34a9f30c4482fb654d4c24bc4c
YuyingRen <renyuying4@gmail.com>
2021-02-12 21:24:31 -0500

    changed direction

--- tutorial2_quantities_and_amounts/main.js
+++ tutorial2_quantities_and_amounts/main.js
@@ -2,19 +2,20 @@
 d3.csv('../data/squirrelActivities.csv', d3.autoType).then(data => {
     console.log('data', data)
 
-const width = window.innerWidth *.4;
-const height = window.innerHeight /3;
+const width = 300 //window.innerWidth *.4;
+const height = 300 //window.innerHeight /3;
 
 const xScale = d3.scaleBand()
     .domain(data.map(d => d.activity))
     .range([0, width])
     .paddingInner(.3)
-//console.log(xScale.domain(), xScale.range())
+console.log(xScale.domain(), xScale.range())
 
 const yScale = d3.scaleLinear()
     .domain([0, d3.max(data, d => d.count)])
     .range([height, 0])
-//console.log(xScale, xScale('eating'))
+console.log(xScale, xScale('eating'))
+console.log(yScale.domain(), yScale.range())
 
 const svg = d3.select("#barchart")
     .append("svg")
@@ -34,6 +35,31 @@ svg.selectAll("rect")
     .attr("height", d => height - yScale(d.count))
     .attr("x", d => xScale(d.activity))
     .attr("y", d => yScale(d.count))
+    .attr("fill", d => myColor(d.activity))
+
+const xScale1 = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.count)])
+    .range([width, 0])
+console.log(xScale1.domain(), xScale1.range())
+const yScale1 = d3.scaleBand()
+    .domain(data.map(d => d.activity))
+    .range([0, height])
+    .paddingInner(.2)
+console.log(yScale1.domain(), yScale1.range())
+console.log(yScale1, yScale1('eating'))
+
+const svg1 = d3.select("#barchart1")
+    .append("svg")
+    .attr("width", height)
+    .attr("height", width)
+
+svg1.selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("height", yScale1.bandwidth())
+    .attr("width", d => width - xScale1(d.count))
+    .attr("y", d => yScale1(d.activity))
+    .attr("x", d => xScale1(d.count))
     .attr("fill", d => myColor(d.activity))
 
 })
