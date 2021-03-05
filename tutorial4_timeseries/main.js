7051aa8ed3cc055b656f9fdbe8e0d439b50f9f7a
YuyingRen <renyuying4@gmail.com>
2021-03-05 15:03:12 -0500

    some updates

--- tutorial4_timeseries/main.js
+++ tutorial4_timeseries/main.js
@@ -4,11 +4,16 @@ const width = window.innerWidth * 0.7,
   margin = { top: 20, bottom: 50, left: 60, right: 40 },
   radius = 5;
 
+//const formatBillions = (num) => d3.format(".2s")(num).replace(/G/, 'B')
+//const formatDate = d3.timeFormat("%Y")
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
 let svg;
 let xScale;
 let yScale;
+let yAxis;
+let xAxisGroup;
+let yAxisGroup;
 
 /* APPLICATION STATE */
 let state = {
@@ -16,31 +21,93 @@ let state = {
   selection: "All", // + YOUR FILTER SELECTION
 };
 
+
 /* LOAD DATA */
 // + SET YOUR DATA PATH
-d3.csv('../data/SUNY_enrollments_y02y19.csv', d3.autoType)
+
+d3.csv('../data/SUNY_enrollments_y02y19.csv', d => {
+
+  return {
+    institution: d.InstitutionSector,
+    race: d.Race,
+    enrollment: +d.Enrollment,
+    year: new Date(+d.Year, 01, 01)
+  }
+})
+
   .then(data => {
     console.log("loaded data:", data);
     state.data = data;
     init();
+
   });
 
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
+
   // + SCALES
-
+  xScale = d3.scaleTime()
+    .domain(d3.extent(state.data, d => d.year))
+    .range([margin.left, width - margin.right])
+  yScale = d3.scaleLinear()
+    .domain(d3.extent(state.data, d =>d.enrollment))
+    .range([height - margin.bottom, margin.top])
   // + AXES
-
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
   // + UI ELEMENT SETUP
+  const dropdown = d3.select('#dropdown')
 
   // add in dropdown options from the unique values in the data
-
+  dropdown.selectAll("option")
+    .data(
+      Array.from(new Set(state.data.map(d => d.race))))
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d)
+  console.log("dropdown: ", dropdown)
   // + SET SELECT ELEMENT'S DEFAULT VALUE (optional)
-
+  
+  dropdown.on("change", event => {
+    console.log("dropdown changed!", event.target.value)
+    state.selection = event.target.value
+    console.log("new state", state)
+    draw();
+  })
   // + CREATE SVG ELEMENT
+  svg = d3.select("#d3-container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
   // + CALL AXES
+  xAxisGroup = svg.append("g")
+    .attr("class", "xAxis")
+    .attr("transform", `translate(${0}, ${height - margin.bottom})`)
+    .call(xAxis)
+  
+  xAxisGroup.append("text")
+    .attr("class", 'xLabel')
+    .attr("transform", `translate(${width / 2}, ${40})`)
+    .attr("text-anchor", "middle")
+    .attr("font-size","14")
+    .attr("fill","black")
+    .text("Year")
+  
+  yAxisGroup = svg.append("g")
+    .attr("class", "yAxis")
+    .attr("transform", `translate(${margin.left}, ${0})`)
+    .call(yAxis)
+
+  yAxisGroup.append("text")
+    .attr("class", 'yLabel')
+    .attr("transform", `translate(${-55}, ${height / 2})`)
+    .attr("writing-mode", 'vertical-rl')
+    .attr("text-anchor", "middle")
+    .attr("font-size","14")
+    .attr("fill","black")
+    .text("Enrollment")
 
   draw(); // calls the draw function
 }
@@ -49,14 +116,55 @@ function init() {
 // we call this everytime there is an update to the data/state
 function draw() {
   // + FILTER DATA BASED ON STATE
+  const filteredData = state.data
+    .filter(d => d.race === state.selection)
 
   // + UPDATE SCALE(S), if needed
 
   // + UPDATE AXIS/AXES, if needed
 
   // + DRAW CIRCLES/LABEL GROUPS, if you decide to
+  const dots = svg
+    .selectAll(".dot")
+    .data(filteredData, d => d.year)
+    .join(
+      enter => enter.append("g")
+        .attr("class", "dot")
+        .attr("transform", d => `translate(${xScale(d.year)}, ${yScale(d.enrollment)})`),
+      update => update
+        .call(update => update.transition()
+          .duration(1000)
+          .attr("transform", d => `translate(${xScale(d.year)}, ${yScale(d.enrollment)})`)
+        ),
+      exit => exit.remove()
+    );
+  dots.selectAll("circle")
+    .data(d => [d]) // pass along data from parent to child
+    .join("circle")
+    .attr("r", radius)
+  
+  dots.selectAll("text")
+    .data(d => [d]) // pass along data from parent to child
+    .join("text")
+    .attr("text-anchor", "end")
+    .text(d => `${d.year}: ${d.enrollment} `)
 
   // + DEFINE LINE GENERATOR FUNCTION
+  const lineGen = d3.line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d.enrollment))
 
   // + DRAW LINE AND/OR AREA
-}
+  
+  svg.selectAll(".line")
+    .data([filteredData]) // data needs to take an []
+    .join("path")
+    .attr("class", 'line')
+    .attr("fill", "none")
+    .attr("stroke", "black")
+    .transition()
+    .duration(1000)
+    .attr("d", d => lineGen(d))
+
+} 
+
